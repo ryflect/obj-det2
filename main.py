63ab821c9a251fc2bf818498f0c19479d0c4b866
ryflect <prateek.mallya@hotmail.com>
2020-03-25 20:47:06 -0400

    'initial'

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import numpy as np 
+import torch 
+import torchvision
 
-print("Hello")
-print(np.zeros(10))
+print(torch.cuda.current_device())
+print(torch.cuda.device(0))
+print(torch.cuda.device_count())
+print(torch.cuda.get_device_name())
+print(torch.cuda.is_available())
