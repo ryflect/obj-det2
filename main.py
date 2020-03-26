4cd369fb430ca3ca616a68c132291605430b0e24
ryflect <prateek.mallya@hotmail.com>
2020-03-25 21:17:57 -0400

    'initial'

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import numpy as np 
+import random
 import torch 
 import torchvision
+import detectron2 
+from detectron2.utils.logger import setup_logger
 
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+# check for cuda availability
 print(torch.cuda.current_device())
 print(torch.cuda.device(0))
 print(torch.cuda.device_count())
