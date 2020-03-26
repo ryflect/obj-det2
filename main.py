46c3e94f0315320ad8ec5e81f4284b78a58d3640
ryflect <prateek.mallya@hotmail.com>
2020-03-25 21:26:23 -0400

    'initial'

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ import torchvision
 import detectron2 
 from detectron2.utils.logger import setup_logger
 
+
 # import some common detectron2 utilities
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
 
 # check for cuda availability
 print(torch.cuda.current_device())
@@ -18,3 +17,4 @@ print(torch.cuda.device(0))
 print(torch.cuda.device_count())
 print(torch.cuda.get_device_name())
 print(torch.cuda.is_available())
+
