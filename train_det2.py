96ce089fbf0fae3efcd39b68146067f6c3e808b7
ryflect <prateek.mallya@hotmail.com>
2020-03-27 14:49:15 -0400

    added inference code for mini

--- train_det2.py
+++ train_det2.py
@@ -7,6 +7,7 @@ import os
 import json 
 import cv2 
 from detectron2.utils.logger import setup_logger
+import matplotlib.pyplot as plt
 
 
 # import some common detectron2 utilities
@@ -15,6 +16,8 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.structures import BoxMode
 from detectron2.engine import DefaultTrainer
+from detectron2.utils.visualizer import ColorMode
+from detectron2.utils.visualizer import Visualizer
 
 # check for cuda availability
 print(torch.cuda.current_device())
@@ -109,3 +112,21 @@ os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 trainer = DefaultTrainer(cfg) 
 trainer.resume_or_load(resume=False)
 trainer.train()
+
+print("Beginning Inference:")
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
+cfg.DATASETS.TEST = ("nusc_train", )
+predictor = DefaultPredictor(cfg)
+# dataset_dicts = get_balloon_dicts("./")
+for d in random.sample(dataset_dicts, 1):    
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=nusc_metadata, 
+                   scale=0.8, 
+                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels
+    )
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    plt.imshow(v.get_image()[:, :, ::-1])
+    plt.savefig("./output/fig.png")
