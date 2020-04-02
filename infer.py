c60ec96f8f679ec77755b62a53988d0927fcd111
ryflect <prateek.mallya@hotmail.com>
2020-04-01 22:03:58 -0400

    multiple image inference

--- infer.py
+++ infer.py
@@ -7,7 +7,6 @@ import os
 import json 
 import cv2 
 from detectron2.utils.logger import setup_logger
-# from train_det2 import conv_catname_to_num, get_dicts
 from matplotlib import pyplot as plt 
 
 # import some common detectron2 utilities
@@ -21,7 +20,7 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import DatasetCatalog, MetadataCatalog
 
 # inference for a single image
-def infer_single_image(image_path, num):
+def infer_single_image(image_path, id_num):
     print("Single Image Inference")
     im = cv2.imread(image_path)
     cfg = get_cfg()
@@ -34,18 +33,85 @@ def infer_single_image(image_path, num):
     v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
     plt.imshow(v.get_image()[:, :, ::-1])
-    plt.savefig("./output/inferred_" + str(num) + ".png")
+    plt.savefig("./output/inferred_" + str(id_num) + ".png")
 
 # save figure that outputs the ground truth as well
-# def show_ground_truth(image_path, annotations_path):
-    # im = cv2.imread(image_path)
-    # DatasetCatalog.register("nusc_train", lambda d=d: get_dicts("./"))
-    # MetadataCatalog.get("nusc_train").set(thing_classes=["barrier", "bicycle", "bus", "car", "construction_vehicle", "motorcycle", "pedestrian", "traffic_cone", "trailer", "truck", "void"])
-    # nusc_metadata = MetadataCatalog.get("nusc_train")
-    # v = Visualizer(im[:, :, ::-1], metadata=nusc_metadata, scale=0.8, )
-    # vis = v.draw_dataset_dict(d)
-    # plt.imshow(vis.get_image()[:, :, ::-1])
-    # plt.savefig("./output/fig_draw.png")
+def show_ground_truth(dict, annotations, id_num):
+    im = cv2.imread(dict["file_name"])
+    v = Visualizer(im[:, :, ::-1], metadata=annotations, scale=0.8, )
+    vis = v.draw_dataset_dict(dict)
+    plt.imshow(vis.get_image()[:, :, ::-1])
+    plt.savefig("./output/ground_" + str(id_num) + ".png")
 
-infer_single_image("./samples/CAM_FRONT/n008-2018-08-30-15-16-55-0400__CAM_FRONT__1535657123112404.jpg", 1)
-# show_ground_truth("./samples/CAM_FRONT/n008-2018-08-30-15-16-55-0400__CAM_FRONT__1535657123112404.jpg")
+def conv_catname_to_num(cat):
+    if cat == "movable_object.barrier":
+        return 0
+    elif cat == "vehicle.bicycle":
+        return 1
+    elif cat == "vehicle.bus.rigid" or cat == "vehicle.bus.bendy":
+        return 2
+    elif cat == "vehicle.car":
+        return 3
+    elif cat == "vehicle.construction":
+        return 4
+    elif cat == "vehicle.motorcycle":
+        return 5
+    elif cat == "human.pedestrian.adult" or cat == "human.pedestrian.police_officer" or cat == "human.pedestrian.child" or cat == "human.pedestrian.construction_worker":
+        return 6
+    elif cat == "movable_object.trafficcone":
+        return 7
+    elif cat == "vehicle.trailer":
+        return 8
+    elif cat == "vehicle.truck":
+        return 9
+    else:
+        return 10
+
+def get_dicts(img_dir):
+    json_file = "./v1.0-mini/image_annotations.json"
+    with open(json_file) as f:
+        image_anns = json.load(f)
+
+    dataset_dicts = []
+    proc_files = []
+    for v in image_anns:
+        if v["sample_data_token"] not in proc_files:
+            proc_files.append(v["sample_data_token"])
+            # print(v["sample_data_token"])
+        else:
+            continue
+            
+        record = {}
+        filename = os.path.join(img_dir, v["filename"])
+        height, width = cv2.imread(filename).shape[:2]
+        
+        record["file_name"] = filename
+        record["image_id"] = v["sample_data_token"]
+        record["height"] = height
+        record["width"] = width
+
+        objs = []
+        for ann in image_anns:
+            if ann["sample_data_token"] == record["image_id"]:
+                obj = {
+                    "bbox": ann["bbox_corners"],
+                    "bbox_mode": BoxMode.XYXY_ABS,
+                    "category_id": conv_catname_to_num(ann["category_name"])
+                }
+                objs.append(obj)
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+    return dataset_dicts
+
+DatasetCatalog.register("nusc_train", lambda d="train": get_dicts("./"))
+MetadataCatalog.get("nusc_train").set(thing_classes=["barrier", "bicycle", "bus", "car", "construction_vehicle", "motorcycle", "pedestrian", "traffic_cone", "trailer", "truck", "void"])
+nusc_metadata = MetadataCatalog.get("nusc_train")
+nusc_dicts = get_dicts("./")
+print(len(nusc_dicts))
+
+# do inference for multiple randomly selected images from the miniset
+count = 0
+for d in random.sample(nusc_dicts, 3):
+    infer_single_image(d["file_name"], count)
+    show_ground_truth(d["file_name"], nusc_metadata, count)
+    count = count + 1
