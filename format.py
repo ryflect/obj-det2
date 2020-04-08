67487f646595bfe1506364b9d4a5db698d1b0744
ryflect <prateek.mallya@hotmail.com>
2020-04-08 00:52:48 -0400

    kitti format read

--- format.py
+++ format.py
@@ -1,11 +1,14 @@
 import json
 import numpy as np 
 import os 
-
+import pandas as pd 
 # fff7106ca1f34c7a83e72ffbc0c0b1b5.txt
 # fff7106ca1f34c7a83e72ffbc0c0b1b5.png
 
 directory = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label_2/"
 
-with open(directory + "fff7106ca1f34c7a83e72ffbc0c0b1b5.txt", 'r') as f:
-    print(f.read())
+# with open(directory + "fff7106ca1f34c7a83e72ffbc0c0b1b5.txt", 'r') as f:
+data = np.loadtxt(directory + "fff7106ca1f34c7a83e72ffbc0c0b1b5.txt")
+print(data)
+print(data[0])
+print(data[0][4:8])
