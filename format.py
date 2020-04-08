bd20cd109babd603046ce7666193bc337afe12f3
ryflect <prateek.mallya@hotmail.com>
2020-04-08 00:46:25 -0400

    kitti format read

--- format.py
+++ format.py
@@ -5,7 +5,7 @@ import os
 # fff7106ca1f34c7a83e72ffbc0c0b1b5.txt
 # fff7106ca1f34c7a83e72ffbc0c0b1b5.png
 
-directory = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label2/"
+directory = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label_2/"
 
 with open(directory + "fff7106ca1f34c7a83e72ffbc0c0b1b5.txt", 'r') as f:
     print(f.read())
