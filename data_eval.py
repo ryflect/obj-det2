b6fdf97fbf0eed585d4f86348df89848440f7ca8
ryflect <prateek.mallya@hotmail.com>
2020-04-10 14:03:50 -0400

    eval the bbox ap

--- data_eval.py
+++ data_eval.py
@@ -4,9 +4,9 @@ def _read_imageset_file(path):
     with open(path, 'r') as f:
         lines = f.readlines()
     return [line[:-2] for line in lines]
-det_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/infer_2/"
+det_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/infer_2"
 dt_annos = kitti.get_label_annos(det_path)
-gt_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label_2/"
+gt_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label_2"
 gt_split_file = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/nusc_val.txt" 
 # val_image_ids = _read_imageset_file(gt_split_file)
 # gt_annos = kitti.get_label_annos(gt_path, val_image_ids)
