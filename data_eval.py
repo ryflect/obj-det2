8d12fbcaf9d770a8da8a27e64b9a0a85996275a8
ryflect <prateek.mallya@hotmail.com>
2020-04-10 14:25:57 -0400

    eval the bbox ap

--- data_eval.py
+++ data_eval.py
@@ -4,13 +4,15 @@ def _read_imageset_file(path):
     with open(path, 'r') as f:
         lines = f.readlines()
     return [line[:-2] for line in lines]
-det_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/infer_2/"
-dt_annos = kitti.get_label_annos(det_path)
-gt_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label_2/"
 gt_split_file = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/nusc_val.txt" 
-# val_image_ids = _read_imageset_file(gt_split_file)
+val_image_ids = _read_imageset_file(gt_split_file)
+print("Val Image IDs: ", val_image_ids)
+det_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/infer_2/"
+dt_annos = kitti.get_label_annos(det_path, val_image_ids)
+gt_path = "/mnt/nfs/scratch1/pmallya/nusc_kitti/val/label_2/"
+
 # gt_annos = kitti.get_label_annos(gt_path, val_image_ids)
-gt_annos = kitti.get_label_annos(gt_path)
+gt_annos = kitti.get_label_annos(gt_path, val_image_ids)
 print(dt_annos)
 print(gt_annos)
 print(get_official_eval_result(gt_annos, dt_annos, 0)) 
