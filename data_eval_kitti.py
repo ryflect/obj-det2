7ef85642b9900254e9478613a38a4490e687b8a9
ryflect <prateek.mallya@hotmail.com>
2020-04-20 13:51:10 -0400

    kitti eval

--- data_eval_kitti.py
+++ data_eval_kitti.py
@@ -4,10 +4,10 @@ from eval import get_official_eval_result, get_coco_eval_result
 def _read_imageset_file(path):
     with open(path, 'r') as f:
         lines = f.readlines()
-    return [line for line in lines]
+    return [line[:-1] for line in lines]
 gt_split_file = "/mnt/nfs/scratch1/pmallya/KITTI/ImageSets/val.txt" 
 val_image_ids = _read_imageset_file(gt_split_file)
-# print("Val Image IDs: ", val_image_ids)
+print("Val Image IDs: ", val_image_ids)
 det_path = "/mnt/nfs/scratch1/pmallya/KITTI/object/training/infer_2/"
 dt_annos = kitti.get_label_annos(det_path, val_image_ids)
 gt_path = "/mnt/nfs/scratch1/pmallya/KITTI/object/training/label_2/"
